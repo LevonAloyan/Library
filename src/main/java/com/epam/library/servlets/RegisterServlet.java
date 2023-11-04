ae915a113e545e227efefe192a4b1cdb75427549
Maruqyan Misha <mishamaruqyan@gmail.com>
2023-11-04 23:56:45 +0400

    Changed managers from Servlet to Spring added H2 db .

--- src/main/java/com/epam/library/servlets/RegisterServlet.java
+++ src/main/java/com/epam/library/servlets/RegisterServlet.java
@@ -3,10 +3,12 @@ package com.epam.library.servlets;
 import com.epam.library.manager.UserManager;
 import com.epam.library.manager.impl.UserManagerImpl;
 import com.epam.library.model.User;
+import com.epam.library.model.UserRole;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,10 +16,11 @@ import java.io.IOException;
 @WebServlet("/register")
 public class RegisterServlet extends GenericServlet {
 
-    private UserManager<Integer,User> userManager;
+    private UserManager<Integer, User> userManager;
 
-    public RegisterServlet (){
-        userManager = new UserManagerImpl();
+    public RegisterServlet() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        userManager = context.getBean("UserManager", UserManagerImpl.class);
     }
 
     @Override
@@ -29,8 +32,8 @@ public class RegisterServlet extends GenericServlet {
         String password = req.getParameter("password");
         String confirmPassword = req.getParameter("confirmPassword");
 
-        if (!password.equals(confirmPassword)){
-            req.setAttribute("passwordMatchError","Password did not match with confirm password");
+        if (!password.equals(confirmPassword)) {
+            req.setAttribute("passwordMatchError", "Password did not match with confirm password");
             req.getRequestDispatcher("/registerPage").forward(req, resp);
             return;
         }
@@ -41,10 +44,9 @@ public class RegisterServlet extends GenericServlet {
         user.setEmail(email);
         user.setPassword(password);
         user.setConfirmPassword(confirmPassword);
+        user.setUserRole(UserRole.ADMIN);
 
         userManager.save(user);
-
         req.getRequestDispatcher("/").forward(req,resp);
-
     }
 }
